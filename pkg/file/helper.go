0eb08c6019bc3b1b8a9b1d67bf247303722826f3
Panos Petropoulos <panospet@learnworlds.com>
2022-11-11 10:33:07 +0200

    small fix to files helper

--- pkg/file/helper.go
+++ pkg/file/helper.go
@@ -24,14 +24,14 @@ func ReadCsv(url string, sourceIsFile bool) ([][]string, error) {
 func ReadCsvFile(filePath string) ([][]string, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
-		return nil, fmt.Errorf("Unable to read input file "+filePath, err)
+		return nil, fmt.Errorf("unable to read input file %s: %s", filePath, err)
 	}
 	defer f.Close()
 
 	csvReader := csv.NewReader(f)
 	records, err := csvReader.ReadAll()
 	if err != nil {
-		return nil, fmt.Errorf("Unable to parse file as CSV for "+filePath, err)
+		return nil, fmt.Errorf("unable to parse file %s as CSV: %s", filePath, err)
 	}
 
 	return records, nil
