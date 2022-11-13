2414ba443e0e4dcf1aa51f21cd78d8ecefa1dc0a
Panos Petropoulos <panospet@learnworlds.com>
2022-11-13 17:46:06 +0200

    small fix to population 2021 name

--- migrations/000006_add_columns_code_populations_to_municipalities.down.sql
+++ migrations/000006_add_columns_code_populations_to_municipalities.down.sql
@@ -3,4 +3,4 @@ DROP INDEX IF EXISTS idx_municipalities_code;
 ALTER TABLE municipalities
     DROP COLUMN IF EXISTS code,
     DROP COLUMN IF EXISTS pop_11,
-    DROP COLUMN IF EXISTS pop_12;
+    DROP COLUMN IF EXISTS pop_21;
--- migrations/000006_add_columns_code_populations_to_municipalities.up.sql
+++ migrations/000006_add_columns_code_populations_to_municipalities.up.sql
@@ -1,7 +1,7 @@
 ALTER TABLE municipalities
     ADD COLUMN IF NOT EXISTS code   VARCHAR(30) UNIQUE,
     ADD COLUMN IF NOT EXISTS pop_11 INTEGER,
-    ADD COLUMN IF NOT EXISTS pop_12 INTEGER;
+    ADD COLUMN IF NOT EXISTS pop_21 INTEGER;
 
 CREATE INDEX IF NOT EXISTS idx_municipalities_code
     ON municipalities (code);
