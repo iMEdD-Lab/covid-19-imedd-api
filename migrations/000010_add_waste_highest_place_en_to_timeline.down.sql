c5966de57c01c51c2220effe2a71854541b7748a
Panos Petropoulos <panospet@learnworlds.com>
2022-12-05 12:27:03 +0200

    add migrations for waste_highest_place_en

---
+++ migrations/000010_add_waste_highest_place_en_to_timeline.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE greece_timeline
+    DROP COLUMN IF EXISTS waste_highest_place_en;
---
+++ migrations/000010_add_waste_highest_place_en_to_timeline.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE greece_timeline
+    ADD COLUMN IF NOT EXISTS waste_highest_place_en VARCHAR(100);
